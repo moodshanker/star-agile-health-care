889cdf2a74f8b15f05cf72e7f18ba6db33de7d46
moodshanker <122299218+moodshanker@users.noreply.github.com>
2023-04-22 11:38:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,40 @@
-provider "aws" {
-region     = "ap-south-1"
-  access_key = "AKIAUY3QFBPP7K24S6PM"
-  secret_key = "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"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "us-east-1
+  access_key= "AKIAUY3QFBPP7K24S6PM"
+  secret_key= "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"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//provider "aws" {
+//region     = "ap-south-1"
+  //access_key = "AKIAUY3QFBPP7K24S6PM"
+  //secret_key = "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"
+//}
