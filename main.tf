7da36ee58b17dd99e5df55792f526218675158d4
moodshanker <122299218+moodshanker@users.noreply.github.com>
2023-04-22 11:50:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,62 @@
-resource "aws_instance" "test-server" {
+resource "aws_instance" "Test-server" {
   ami           = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.medium" 
-  key_name = "frstkey"
+  availability_zone      = "us-east-1a"
   vpc_security_group_ids= ["sg-0347106857582df29"]
-  connection {
-    type     = "ssh"
-    user     = "ubuntu"
-    private_key = file("frstkey.pem")
-    # host     = self.public_ip
-    host = aws_instance.instance.public_ip
-   
-  }
-  provisioner "remote-exec" {
-    inline = [ "echo 'wait to start instance' "]
-  }
+  key_name = "frstkey"
   tags = {
-    Name = "test-server"
+    name = "kubernetes_instance"
   }
-  provisioner "local-exec" {
-        command = " echo ${aws_instance.test-server.public_ip} > inventory "
-  }
+  
    provisioner "remote-exec" {
      inline = [
-               "sudo apt upadte -y",
-               "sudo apt install docker.io -y",
-               "sudo snap install microk8s --clasic",
-               "sudo sleep 30",
-               "sudo microk8s status",
-               "sudo microk8s kubectl create deployment medicure-deploy --image=shankerchauhan/projects:healthcare1",
-               "sudo microk8s kubectl expose deployment medicure-deploy --port=8082 --type=NodePort",
-               "sudo microk8s kubectl get svc",
-               "sudo echo public IP Address of the Instance",
-               "sudo curl http://checkip.amazonaws.com",
+      "sudo apt update -y",
+      "sudo apt install docker.io -y",
+      "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
+      "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+      "sudo chmod +x /home/ubuntu/kubectl",
+      "sudo cp kubectl /usr/local/bin/kubectl",
+      "sudo usermod -aG docker ubuntu"
        ]
-   } 
+     connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("./frstkey.pem")
+    }
+  }
 }
+  //connection {
+    //type     = "ssh"
+    //user     = "ubuntu"
+    //private_key = file("frstkey.pem")
+    // host     = self.public_ip
+    //host = aws_instance.instance.public_ip
+   
+ // }
+ 
+   // inline = [ "echo 'wait to start instance' "]
+  //}
+  //tags = {
+    //Name = "test-server"
+  //}
+  //provisioner "local-exec" {
+        //command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  //}
+   //provisioner "remote-exec" {
+     //inline = [
+            //   "sudo apt upadte -y",
+              // "sudo apt install docker.io -y",
+               //"sudo snap install microk8s --clasic",
+               //"sudo sleep 30",
+               //"sudo microk8s status",
+               //"sudo microk8s kubectl create deployment medicure-deploy --image=shankerchauhan/projects:healthcare1",
+               //"sudo microk8s kubectl expose deployment medicure-deploy --port=8082 --type=NodePort",
+               //"sudo microk8s kubectl get svc",
+               //"sudo echo public IP Address of the Instance",
+               //"sudo curl http://checkip.amazonaws.com",
+       //]
+   //} 
+//}
